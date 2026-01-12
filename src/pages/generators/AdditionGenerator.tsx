9f3e274bf874b4c35567aa517a935abf74e81508
Syed Auntor <83162187+syedauntor@users.noreply.github.com>
2026-01-13 01:31:01 +0600

    Updated AdditionGenerator.tsx

--- src/pages/generators/AdditionGenerator.tsx
+++ src/pages/generators/AdditionGenerator.tsx
@@ -200,7 +200,7 @@ export function AdditionGenerator() {
                       </div>
 
                       <div className="flex items-center justify-center flex-1">
-                        <div className={`grid ${orientation === 'horizontal' ? 'grid-cols-2 gap-y-5 gap-x-8' : 'grid-cols-4 gap-y-5 gap-x-3'}`}>
+                        <div className={`grid ${orientation === 'horizontal' ? 'grid-cols-2 gap-y-8 gap-x-16' : 'grid-cols-4 gap-y-8 gap-x-8'}`}>
                           {problems.map((problem, index) => (
                             <div key={index} className="text-center">
                               {orientation === 'horizontal' ? (
