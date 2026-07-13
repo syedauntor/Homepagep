f5c16e9d5d62b5d654cba3d19a263447ca11fc82
Syed Auntor <83162187+syedauntor@users.noreply.github.com>
2026-07-13 13:04:28 +0600

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,6 @@ import SettingsPage from './pages/admin/SettingsPage';
 import AuthorsPage from './pages/admin/AuthorsPage';
 import PagesManagement from './pages/admin/PagesManagement';
 import { DynamicPage } from './pages/DynamicPage';
-import { DownloadThemePage } from './pages/DownloadThemePage';
 
 function App() {
   return (
@@ -80,7 +79,6 @@ function App() {
                     <Route path="/blog/:id" element={<BlogPostPage />} />
                     <Route path="/category" element={<CategoryPage />} />
                     <Route path="/category/:slug" element={<CategoryPage />} />
-                    <Route path="/download-theme" element={<DownloadThemePage />} />
                     <Route path="/about" element={<AboutPage />} />
                     <Route path="/contact" element={<ContactPage />} />
                     <Route path="/:slug" element={<DynamicPage />} />
